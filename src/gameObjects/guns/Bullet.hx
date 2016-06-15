495867d1500ebda2a4c4a664efeb910814b14915
nachobenedetto <ignaciobenedetto@gmail.com>
2016-06-15 10:28:58 -0300

    Limpieza de codigo repetido.

--- src/gameObjects/guns/Bullet.hx
+++ src/gameObjects/guns/Bullet.hx
@@ -17,21 +17,21 @@ class Bullet extends FlxSprite
 	public 	var bulletDamage:Int;
 	private static inline var bulletLifeTime: Int = 4000;
 	private var bulletStartTime:Int = 0;
-	
+
 	//Target Coords.
 	private var targetX:Float;
 	private var targetY:Float;
-	
-	
 
-	public function new(aBulletSpeed:Int, aBulletDamage:Int) 
+
+
+	public function new(aBulletSpeed:Int, aBulletDamage:Int)
 	{
 		super(0, 0);
 		bulletSpeed = aBulletSpeed;
 		bulletDamage = aBulletDamage;
 		alive = false;
 	}
-	
+
 	override public function update (elapsed: Float):Void
 	{
 		super.update(elapsed);
@@ -45,33 +45,28 @@ class Bullet extends FlxSprite
 		switch bType
 		{
 			case 1: loadGraphic(Assets.getBitmapData("img/bullets/Bullets001.png"), true, 11, 11);
-					animation.add("idle", [0, 1, 2, 1, ], 20, true);
-					animation.play("idle");
 			case 2: loadGraphic(Assets.getBitmapData("img/bullets/Bullets002.png"), true, 11, 11);
-					animation.add("idle", [0, 1, 2, 1, ], 20, true);
-					animation.play("idle");
 			case 3: loadGraphic(Assets.getBitmapData("img/bullets/Bullets003.png"), true, 25, 25);
-					animation.add("idle", [0, 1, 2, 1, ], 20, true);
-					animation.play("idle");
 			default: loadGraphic(Assets.getBitmapData("img/bullets/Bullets001.png"), true, 11, 11);
-					animation.add("idle", [0, 1, 2, 1, ], 20, true);
-					animation.play("idle");
 		}
+		animation.add("idle", [0, 1, 2, 1, ], 20, true);
+		animation.play("idle");
+		
 		bulletStartTime = Lib.getTimer();
 		var dX:Float = aTargetX - aOrX;
 		var dY:Float = aTargetY - aOrY;
-		var length:Float = Math.sqrt(dX * dX + dY * dY); 
+		var length:Float = Math.sqrt(dX * dX + dY * dY);
 		dX /= length;
 		dY /= length;
 		velocity.x = dX * bulletSpeed;
 		velocity.y = dY * bulletSpeed;
 	}
-		
-	override public function reset(aX:Float, aY:Float):Void 
+
+	override public function reset(aX:Float, aY:Float):Void
 	{
 		super.reset(aX, aY);
 	}
-	
-	
-	
+
+
+
 }
